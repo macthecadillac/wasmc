21cc98384d31cb0e69db6235e691615dd42df721
Mac Lee <macthecadillac@gmail.com>
2021-05-17 10:49:51 -0700

    Qualify imports for every LLVM module

--- src/Lib.hs
+++ src/Lib.hs
@@ -13,34 +13,34 @@ import Debug.Trace
 import qualified Language.Wasm as Wasm
 import qualified Language.Wasm.Structure as S
 import qualified Language.Wasm.Structure (Function(..))
-import qualified Language.Wasm.Lexer as X
 
 -- LLVM Stuff
-import LLVM.AST
-import qualified LLVM.AST.Constant as C
-import qualified LLVM.AST.Float as F
-import LLVM.AST.IntegerPredicate as P
-import LLVM.AST.Name
-import LLVM.AST.Global
-import LLVM.AST.Type
+import qualified LLVM.AST as AST
+import qualified LLVM.AST.Constant as Constant
+import qualified LLVM.AST.Float as Float
+import qualified LLVM.AST.IntegerPredicate as Pred
+import qualified LLVM.AST.Name as Name
+import qualified LLVM.AST.Global as Global
+import qualified LLVM.AST.Type as Type
 
 import Numeric.Natural
 import Utils (newName, splitWhen)
 
 data WasmModST = WasmModST { startFunctionIndex :: Maybe Natural
-                           , currentFunction :: [BasicBlock]
-                           , currentBasicBlockInstrs :: [Named Instruction]
-                           , operandStack :: [Operand]
+                           , operandStack :: [AST.Operand]
                            , currentIdentifierNumber :: Natural
                            , functionTypes :: M.Map Natural S.FuncType }
                            deriving (Show)
 
-data LLVMInstr = B BinOp | U UnOp deriving (Show)
+data LLVMInstr = B BinOp
+               | U UnOp
+               | Call Natural
+               deriving (Show)
 
 --           tag   bitsize/fp type   type  
 data BinOp = BBOOI Word32            BBOOI
            | BOOI  Word32            BOOI
-           | FOOI  FloatingPointType FOOI
+           | FOOI  Type.FloatingPointType FOOI
            | OOI   Word32            OOI
            | IOOI                    IOOI
 
@@ -61,24 +61,24 @@ data LLVMTerm = Ret
 instance Show LLVMTerm where
   show Lib.Ret = "Ret"
 
-type BOI =  Bool -> Operand -> InstructionMetadata -> Instruction
-type BOOI =  Bool -> Operand -> Operand -> InstructionMetadata -> Instruction
-type BBOOI = Bool -> Bool -> Operand -> Operand -> InstructionMetadata -> Instruction
-type FOOI = FastMathFlags -> Operand -> Operand -> InstructionMetadata -> Instruction
-type IOOI =  IntegerPredicate -> Operand -> Operand -> InstructionMetadata -> Instruction
-type OOI = Operand -> Operand -> InstructionMetadata -> Instruction
-type MOI = Maybe Operand -> InstructionMetadata -> Terminator
+type BOI =  Bool -> AST.Operand -> AST.InstructionMetadata -> AST.Instruction
+type BOOI =  Bool -> AST.Operand -> AST.Operand -> AST.InstructionMetadata -> AST.Instruction
+type BBOOI = Bool -> Bool -> AST.Operand -> AST.Operand -> AST.InstructionMetadata -> AST.Instruction
+type FOOI = AST.FastMathFlags -> AST.Operand -> AST.Operand -> AST.InstructionMetadata -> AST.Instruction
+type IOOI =  Pred.IntegerPredicate -> AST.Operand -> AST.Operand -> AST.InstructionMetadata -> AST.Instruction
+type OOI = AST.Operand -> AST.Operand -> AST.InstructionMetadata -> AST.Instruction
+type MOI = Maybe AST.Operand -> AST.InstructionMetadata -> AST.Terminator
 
 data LLVMObj = Instr LLVMInstr
-             | Op Operand
+             | Op AST.Operand
              | Term LLVMTerm
              deriving (Show)
 
-iop :: BBOOI -> Operand -> Operand -> Instruction
+iop :: BBOOI -> AST.Operand -> AST.Operand -> AST.Instruction
 iop op a b = op False False a b []
 
-fop :: FOOI -> Operand -> Operand -> Instruction
-fop op a b = op noFastMathFlags a b []
+fop :: FOOI -> AST.Operand -> AST.Operand -> AST.Instruction
+fop op a b = op AST.noFastMathFlags a b []
 
 isLLVMInstr :: LLVMObj -> Bool
 isLLVMInstr (Instr _) = True
@@ -92,7 +92,7 @@ isLLVMTerm :: LLVMObj -> Bool
 isLLVMTerm (Term _) = True
 isLLVMTerm _        = False
 
-unwrapOp :: LLVMObj -> Operand 
+unwrapOp :: LLVMObj -> AST.Operand 
 unwrapOp (Op op) = op
 unwrapOp _       = error "not an operand"
 
@@ -101,84 +101,86 @@ unwrapTerm (Term t) = t
 unwrapTerm _        = error "not a terminator"
 
 compileInstr :: S.Instruction Natural -> LLVMObj
-compileInstr (S.I32Const n) = Op $ ConstantOperand $ C.Int 32 $ fromIntegral n
-compileInstr (S.I64Const n) = Op $ ConstantOperand $ C.Int 64 $ fromIntegral n
-compileInstr (S.F32Const n) = Op $ ConstantOperand $ C.Float $ F.Single n
-compileInstr (S.F64Const n) = Op $ ConstantOperand $ C.Float $ F.Double n
+compileInstr (S.I32Const n) = Op $ AST.ConstantOperand $ Constant.Int 32 $ fromIntegral n
+compileInstr (S.I64Const n) = Op $ AST.ConstantOperand $ Constant.Int 64 $ fromIntegral n
+compileInstr (S.F32Const n) = Op $ AST.ConstantOperand $ Constant.Float $ Float.Single n
+compileInstr (S.F64Const n) = Op $ AST.ConstantOperand $ Constant.Float $ Float.Double n
 
 -- Binary Operations(IBinOPs)
 -- TODO: should use helper functions instead of explicitly matching the bitsize
-compileInstr (S.IBinOp S.BS32 S.IAdd)  = Instr $ B $ BBOOI 32 Add
-compileInstr (S.IBinOp S.BS32 S.ISub)  = Instr $ B $ BBOOI 32 Sub 
-compileInstr (S.IBinOp S.BS32 S.IMul)  = Instr $ B $ BBOOI 32 Mul
-compileInstr (S.IBinOp S.BS32 S.IDivS) = Instr $ B $ BOOI  32 SDiv
-compileInstr (S.IBinOp S.BS32 S.IRemS) = Instr $ B $ OOI   32 SRem
-compileInstr (S.IBinOp S.BS32 S.IAnd)  = Instr $ B $ OOI   32 And 
-compileInstr (S.IBinOp S.BS32 S.IOr)   = Instr $ B $ OOI   32 Or
-compileInstr (S.IBinOp S.BS32 S.IXor)  = Instr $ B $ OOI   32 Xor
-compileInstr (S.IBinOp S.BS32 S.IShl)  = Instr $ B $ BBOOI 32 Shl
+compileInstr (S.IBinOp S.BS32 S.IAdd)  = Instr $ B $ BBOOI 32 AST.Add
+compileInstr (S.IBinOp S.BS32 S.ISub)  = Instr $ B $ BBOOI 32 AST.Sub 
+compileInstr (S.IBinOp S.BS32 S.IMul)  = Instr $ B $ BBOOI 32 AST.Mul
+compileInstr (S.IBinOp S.BS32 S.IDivS) = Instr $ B $ BOOI  32 AST.SDiv
+compileInstr (S.IBinOp S.BS32 S.IRemS) = Instr $ B $ OOI   32 AST.SRem
+compileInstr (S.IBinOp S.BS32 S.IAnd)  = Instr $ B $ OOI   32 AST.And 
+compileInstr (S.IBinOp S.BS32 S.IOr)   = Instr $ B $ OOI   32 AST.Or
+compileInstr (S.IBinOp S.BS32 S.IXor)  = Instr $ B $ OOI   32 AST.Xor
+compileInstr (S.IBinOp S.BS32 S.IShl)  = Instr $ B $ BBOOI 32 AST.Shl
 
-compileInstr (S.IBinOp S.BS64 S.IAdd)  = Instr $ B $ BBOOI 64 Add
-compileInstr (S.IBinOp S.BS64 S.ISub)  = Instr $ B $ BBOOI 64 Sub 
-compileInstr (S.IBinOp S.BS64 S.IMul)  = Instr $ B $ BBOOI 64 Mul
-compileInstr (S.IBinOp S.BS64 S.IDivS) = Instr $ B $ BOOI  64 SDiv
-compileInstr (S.IBinOp S.BS64 S.IRemS) = Instr $ B $ OOI   64 SRem
-compileInstr (S.IBinOp S.BS64 S.IAnd)  = Instr $ B $ OOI   64 And 
-compileInstr (S.IBinOp S.BS64 S.IOr)   = Instr $ B $ OOI   64 Or
-compileInstr (S.IBinOp S.BS64 S.IXor)  = Instr $ B $ OOI   64 Xor
-compileInstr (S.IBinOp S.BS64 S.IShl)  = Instr $ B $ BBOOI 64 Shl
+compileInstr (S.IBinOp S.BS64 S.IAdd)  = Instr $ B $ BBOOI 64 AST.Add
+compileInstr (S.IBinOp S.BS64 S.ISub)  = Instr $ B $ BBOOI 64 AST.Sub 
+compileInstr (S.IBinOp S.BS64 S.IMul)  = Instr $ B $ BBOOI 64 AST.Mul
+compileInstr (S.IBinOp S.BS64 S.IDivS) = Instr $ B $ BOOI  64 AST.SDiv
+compileInstr (S.IBinOp S.BS64 S.IRemS) = Instr $ B $ OOI   64 AST.SRem
+compileInstr (S.IBinOp S.BS64 S.IAnd)  = Instr $ B $ OOI   64 AST.And 
+compileInstr (S.IBinOp S.BS64 S.IOr)   = Instr $ B $ OOI   64 AST.Or
+compileInstr (S.IBinOp S.BS64 S.IXor)  = Instr $ B $ OOI   64 AST.Xor
+compileInstr (S.IBinOp S.BS64 S.IShl)  = Instr $ B $ BBOOI 64 AST.Shl
 -- count leading zeros, trailing zeros, popped vals (IUnOPs)//////TO FIX
 --compileInstr (S.IUnOp _ S.IClz) = Instr $ -- Clz
 --compileInstr (S.IUnOp _ S.ICtz) = Instr $ -- Ctz
 --compileInstr (S.IUnOp _ S.IPopcnt) = Instr $ -- Popcnt
 -- Relational Operations(IRelOPs) IEq	 INe	 ILtU	 ILtS	 IGtU	 IGtS	 ILeU	 ILeS	 IGeU	 IGeS
-compileInstr (S.IRelOp S.BS32 S.IEq)  = Instr $ B $ OOI 32 $ ICmp P.EQ
-compileInstr (S.IRelOp S.BS32 S.INe)  = Instr $ B $ OOI 32 $ ICmp P.NE
-compileInstr (S.IRelOp S.BS32 S.ILtU) = Instr $ B $ OOI 32 $ ICmp P.ULT
-compileInstr (S.IRelOp S.BS32 S.ILtS) = Instr $ B $ OOI 32 $ ICmp P.SLT
-compileInstr (S.IRelOp S.BS32 S.IGtU) = Instr $ B $ OOI 32 $ ICmp P.UGT
-compileInstr (S.IRelOp S.BS32 S.IGtS) = Instr $ B $ OOI 32 $ ICmp P.SGT
-compileInstr (S.IRelOp S.BS32 S.ILeU) = Instr $ B $ OOI 32 $ ICmp P.ULE
-compileInstr (S.IRelOp S.BS32 S.ILeS) = Instr $ B $ OOI 32 $ ICmp P.SLE
-compileInstr (S.IRelOp S.BS32 S.IGeU) = Instr $ B $ OOI 32 $ ICmp P.UGE
-compileInstr (S.IRelOp S.BS32 S.IGeS) = Instr $ B $ OOI 32 $ ICmp P.SGE
+compileInstr (S.IRelOp S.BS32 S.IEq)  = Instr $ B $ OOI 32 $ AST.ICmp Pred.EQ
+compileInstr (S.IRelOp S.BS32 S.INe)  = Instr $ B $ OOI 32 $ AST.ICmp Pred.NE
+compileInstr (S.IRelOp S.BS32 S.ILtU) = Instr $ B $ OOI 32 $ AST.ICmp Pred.ULT
+compileInstr (S.IRelOp S.BS32 S.ILtS) = Instr $ B $ OOI 32 $ AST.ICmp Pred.SLT
+compileInstr (S.IRelOp S.BS32 S.IGtU) = Instr $ B $ OOI 32 $ AST.ICmp Pred.UGT
+compileInstr (S.IRelOp S.BS32 S.IGtS) = Instr $ B $ OOI 32 $ AST.ICmp Pred.SGT
+compileInstr (S.IRelOp S.BS32 S.ILeU) = Instr $ B $ OOI 32 $ AST.ICmp Pred.ULE
+compileInstr (S.IRelOp S.BS32 S.ILeS) = Instr $ B $ OOI 32 $ AST.ICmp Pred.SLE
+compileInstr (S.IRelOp S.BS32 S.IGeU) = Instr $ B $ OOI 32 $ AST.ICmp Pred.UGE
+compileInstr (S.IRelOp S.BS32 S.IGeS) = Instr $ B $ OOI 32 $ AST.ICmp Pred.SGE
 
-compileInstr (S.IRelOp S.BS64 S.IEq)  = Instr $ B $ OOI 64 $ ICmp P.EQ
-compileInstr (S.IRelOp S.BS64 S.INe)  = Instr $ B $ OOI 64 $ ICmp P.NE
-compileInstr (S.IRelOp S.BS64 S.ILtU) = Instr $ B $ OOI 64 $ ICmp P.ULT
-compileInstr (S.IRelOp S.BS64 S.ILtS) = Instr $ B $ OOI 64 $ ICmp P.SLT
-compileInstr (S.IRelOp S.BS64 S.IGtU) = Instr $ B $ OOI 64 $ ICmp P.UGT
-compileInstr (S.IRelOp S.BS64 S.IGtS) = Instr $ B $ OOI 64 $ ICmp P.SGT
-compileInstr (S.IRelOp S.BS64 S.ILeU) = Instr $ B $ OOI 64 $ ICmp P.ULE
-compileInstr (S.IRelOp S.BS64 S.ILeS) = Instr $ B $ OOI 64 $ ICmp P.SLE
-compileInstr (S.IRelOp S.BS64 S.IGeU) = Instr $ B $ OOI 64 $ ICmp P.UGE
-compileInstr (S.IRelOp S.BS64 S.IGeS) = Instr $ B $ OOI 64 $ ICmp P.SGE
+compileInstr (S.IRelOp S.BS64 S.IEq)  = Instr $ B $ OOI 64 $ AST.ICmp Pred.EQ
+compileInstr (S.IRelOp S.BS64 S.INe)  = Instr $ B $ OOI 64 $ AST.ICmp Pred.NE
+compileInstr (S.IRelOp S.BS64 S.ILtU) = Instr $ B $ OOI 64 $ AST.ICmp Pred.ULT
+compileInstr (S.IRelOp S.BS64 S.ILtS) = Instr $ B $ OOI 64 $ AST.ICmp Pred.SLT
+compileInstr (S.IRelOp S.BS64 S.IGtU) = Instr $ B $ OOI 64 $ AST.ICmp Pred.UGT
+compileInstr (S.IRelOp S.BS64 S.IGtS) = Instr $ B $ OOI 64 $ AST.ICmp Pred.SGT
+compileInstr (S.IRelOp S.BS64 S.ILeU) = Instr $ B $ OOI 64 $ AST.ICmp Pred.ULE
+compileInstr (S.IRelOp S.BS64 S.ILeS) = Instr $ B $ OOI 64 $ AST.ICmp Pred.SLE
+compileInstr (S.IRelOp S.BS64 S.IGeU) = Instr $ B $ OOI 64 $ AST.ICmp Pred.UGE
+compileInstr (S.IRelOp S.BS64 S.IGeS) = Instr $ B $ OOI 64 $ AST.ICmp Pred.SGE
 -- FUn Operations(FUnOPs) FAbs	 FNeg	 FCeil	 FFloor	 FTrunc	 FNearest	 FSqrt
 --compileInstr (S.FUnOp _ S.FAbs) = Instr $ IOOI Abs
+
+compileInstr (S.Call i)               = Instr $ Call $ fromIntegral i
 
 -- Terminators (return, br etc)
 compileInstr S.Return = Term Lib.Ret
 compileInstr instr = error $ "Not implemented in compileInstr: " ++ show instr
 
-compileType :: S.ValueType -> Type
-compileType S.I32 = IntegerType 32
-compileType S.I64 = IntegerType 64
-compileType S.F32 = FloatingPointType FloatFP
-compileType S.F64 = FloatingPointType DoubleFP
+compileType :: S.ValueType -> Type.Type
+compileType S.I32 = Type.IntegerType 32
+compileType S.I64 = Type.IntegerType 64
+compileType S.F32 = Type.FloatingPointType Type.FloatFP
+compileType S.F64 = Type.FloatingPointType Type.DoubleFP
     
-buildBasicBlock :: Name -> LLVMTerm -> [LLVMObj] -> ExceptT String (State WasmModST) BasicBlock
-buildBasicBlock name term llvmObjs = BasicBlock name <$> llvmInstrs <*> makeTerminator term
+buildBasicBlock :: Name.Name -> LLVMTerm -> [LLVMObj] -> ExceptT String (State WasmModST) Global.BasicBlock
+buildBasicBlock name term llvmObjs = Global.BasicBlock name <$> llvmInstrs <*> makeTerminator term
   where
     chunks = splitWhen (\x -> isLLVMInstr x || isLLVMTerm x) llvmObjs
     llvmInstrs = L.concat <$> traverse munch chunks
 
-    makeTerminator :: LLVMTerm -> ExceptT String (State WasmModST) (Named Terminator)
+    makeTerminator :: LLVMTerm -> ExceptT String (State WasmModST) (AST.Named AST.Terminator)
     makeTerminator Lib.Ret = do
       retVal <- stackToRetVal . operandStack <$> get
       modify (\env -> env { operandStack = [] })
-      pure $ Do $ LLVM.AST.Ret (Just retVal) []
+      pure $ AST.Do $ AST.Ret (Just retVal) []
 
-    stackToRetVal []  = LocalReference VoidType "void"
+    stackToRetVal []  = AST.LocalReference Type.VoidType "void"
     stackToRetVal [x] = x
     stackToRetVal l   = error "add support for multiple return vals" -- TODO: support multiple return values
 
@@ -188,20 +190,20 @@ buildBasicBlock name term llvmObjs = BasicBlock name <$> llvmInstrs <*> makeTerm
     -- sub a b vs sub b a
     -- doesnt the instr come after the ops in wasm?
     -- the order of a and b might need to be switched
-    munch :: [LLVMObj] -> ExceptT String (State WasmModST) [Named Instruction]
+    munch :: [LLVMObj] -> ExceptT String (State WasmModST) [AST.Named AST.Instruction]
     munch = munchBackwards . L.reverse
       where
         munchBackwards [] = pure []
         munchBackwards (Instr (B op):Op a:Op b:rest) = do
           putOperandsOnStack rest
           identifier <- identify $ B op
-          pure [identifier := buildBinOp op a b]
+          pure [identifier AST.:= buildBinOp op a b]
         munchBackwards [Instr (B op), Op a] = do
           env        <- get
           (b, rest)  <- maybe (fail "not enough operands") pure $ L.uncons $ operandStack env
           put $ env { operandStack = rest }
           identifier <- identify $ B op
-          pure [identifier := buildBinOp op a b]
+          pure [identifier AST.:= buildBinOp op a b]
         munchBackwards [Instr (B op)] = do
           env          <- get
           (a, b, rest) <- case operandStack env of
@@ -209,26 +211,26 @@ buildBasicBlock name term llvmObjs = BasicBlock name <$> llvmInstrs <*> makeTerm
             _        -> fail "not enough operands"
           put $ env { operandStack = rest }
           identifier   <- identify $ B op
-          pure [identifier := buildBinOp op a b]
+          pure [identifier AST.:= buildBinOp op a b]
         munchBackwards l
           | all isLLVMOp l = putOperandsOnStack l *> pure []
-          | otherwise      = error $ "Instruction sequence not implemented: " ++ show l -- FIXME: complete me!!
+          | otherwise      = error $ "AST.Instruction sequence not implemented: " ++ show l -- FIXME: complete me!!
 
-    buildBinOp :: BinOp -> Operand -> Operand -> Instruction
+    buildBinOp :: BinOp -> AST.Operand -> AST.Operand -> AST.Instruction
     buildBinOp (BBOOI _ op) = iop op
     buildBinOp (FOOI  _ op) = fop op
     buildBinOp o          = error $ "Not implemented for binop type: " ++ show o
 
-    identify (B (BBOOI bs _)) = newIdentifier $ IntegerType bs
-    identify (B (BOOI  bs _)) = newIdentifier $ IntegerType bs
-    identify (B (OOI   bs _)) = newIdentifier $ IntegerType bs
+    identify (B (BBOOI bs _)) = newIdentifier $ Type.IntegerType bs
+    identify (B (BOOI  bs _)) = newIdentifier $ Type.IntegerType bs
+    identify (B (OOI   bs _)) = newIdentifier $ Type.IntegerType bs
     identify _                = error "not implmented"
 
-    newIdentifier :: Type -> ExceptT String (State WasmModST) Name
+    newIdentifier :: Type.Type -> ExceptT String (State WasmModST) Name.Name
     newIdentifier idType = do
       WasmModST { currentIdentifierNumber, operandStack } <- get
       let name       = newName "ident" $ currentIdentifierNumber + 1
-          identifier = LocalReference idType name
+          identifier = AST.LocalReference idType name
       modify (\env -> env {
          currentIdentifierNumber = currentIdentifierNumber + 1,
          operandStack = identifier : operandStack
@@ -242,7 +244,7 @@ buildBasicBlock name term llvmObjs = BasicBlock name <$> llvmInstrs <*> makeTerm
                          f env = env { operandStack = newOps ++ operandStack env }
                          newOps = unwrapOp <$> l
 
-compileFunction :: Natural -> S.Function -> ExceptT String (State WasmModST) Global
+compileFunction :: Natural -> S.Function -> ExceptT String (State WasmModST) Global.Global
 compileFunction indx func = do
   WasmModST { startFunctionIndex, operandStack, functionTypes } <- get
   let funcType   = functionTypes M.! (S.funcType func)
@@ -257,19 +259,22 @@ compileFunction indx func = do
         $ const "main" <$> (guard . (==indx) =<< startFunctionIndex)
   modify (\env -> env { currentIdentifierNumber = fromIntegral $ L.length paramList })
   basicBlocks <- traverse (\(n, t, o) -> buildBasicBlock n t o) namedBlks
-  pure $ functionDefaults { basicBlocks, name, returnType, parameters }
+  pure $ Global.functionDefaults { Global.basicBlocks
+                                 , Global.name
+                                 , Global.returnType
+                                 , Global.parameters }
   where
     llvmObjs = compileInstr <$> Language.Wasm.Structure.body func
 
     assignName (n, (t, instrs)) = (newName "block" n, t, instrs)
 
-    compileRetType []  = VoidType
+    compileRetType []  = Type.VoidType
     compileRetType [t] = compileType t
-    compileRetType l   = StructureType True $ compileType <$> l
+    compileRetType l   = Type.StructureType True $ compileType <$> l
 
     buildParamList l = do
       (i, t) <- zip [0..] l
-      pure $ Parameter (compileType t) (newName "ident" i) []
+      pure $ Global.Parameter (compileType t) (newName "ident" i) []
 
     splitTerm :: [LLVMObj] -> (LLVMTerm, [LLVMObj])
     splitTerm []            = error "empty block"  -- this would be a bug
@@ -283,23 +288,21 @@ compileFunction indx func = do
 -- TODO: datas
 -- TODO: imports
 -- TODO: exports
-compileModule :: S.Module -> Either String Module
+compileModule :: S.Module -> Either String AST.Module
 compileModule wasmMod = do
   globalDefs <- buildGlobalDefs wasmMod
-  let moduleDefinitions = GlobalDefinition <$> globalDefs
-  pure $ defaultModule
-    { moduleName = "basic",
-      moduleDefinitions
+  pure $ AST.defaultModule
+    { AST.moduleName = "basic",
+      AST.moduleDefinitions = [AST.GlobalDefinition def | def <- globalDefs]
     }
   where
     startFunctionIndex = (\(S.StartFunction n) -> n) <$> S.start wasmMod
     initModST = WasmModST { startFunctionIndex
-                          , currentFunction = []
-                          , currentBasicBlockInstrs = []
                           , operandStack = []
                           , currentIdentifierNumber = 0
                           , functionTypes = M.fromList $ zip [0..] $ S.types wasmMod
                           }
+    -- extract functions from a WASM module and compile them
     buildGlobalDefs = flip evalState initModST
                     . runExceptT
                     . traverse (uncurry compileFunction)
