f20955940fa70e3478169361408aacea45095137
Mac Lee <macthecadillac@gmail.com>
2021-06-02 05:02:24 -0700

    file to run game

--- demo/runGame.sh
+++ demo/runGame.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-rm *.o exe
+rm *.o life
 clang -c conway.c
-stack run -- test.wat
+# stack run -- test.wat
 llc-9 --filetype=obj test.ll
-clang conway.o test.o -lm -o life
-./life 
+clang conway.o test.o -lm -lncurses -o life
+./life
+# ./life > gameoutput.txt
+# cat gameoutput.txt
